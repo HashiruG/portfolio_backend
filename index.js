8e3f42c3329d3372dc4e617b0bbd3eb1229e0e3d
HashiruG <hashiruhgunathilake@gmail.com>
2024-05-21 09:36:24 +0530

    Implement CRUD operations

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const Project = require("./project");
 const Blog = require("./blog");
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
 
@@ -31,6 +32,46 @@ app.get("/blogs", async (req, res) => {
       res.status(500).json({ message: err.message });
     }
   });
+
+  app.post("/projects", async (req, res) => {
+    const project = new Project(req.body);
+    try {
+      const newProject = await project.save();
+      res.status(201).json(newProject);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  });
+
+  app.patch("/projects/:id", async (req, res) => {
+    try{
+    const project = await Project.findById(req.params.id);
+ 
+      if(project){
+        project.set(req.body);
+        const updatedProject = await project.save();
+        res.send(updatedProject);
+      }else{
+        res.status(404).json({message: "Project not found"})
+      }
+    }catch(err){
+      res.status(500).json({message: err.message});
+    }
+  })
+
+  app.delete("/projects/:id", async (req, res) => {
+    try {
+      const project = await Project.findOneAndDelete({ _id: req.params.id });
+      if (project) {
+        res.status(200).json({ message: "Successfully deleted" });
+      } else {
+        res.status(404).json({ message: "No match found" });
+      }
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
